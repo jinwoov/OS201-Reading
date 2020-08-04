a2db24ae9ac25af3bc6190833093a84cb77d2194
jinwoov <jinwoov@gmail.com>
2020-08-03 17:25:15 -0700

    changes made on 08/03/2020 17:25:15

--- Lab/Lab06.ps1
+++ Lab/Lab06.ps1
@@ -14,28 +14,35 @@ $NETWORK = Get-WmiObject Win32_NetworkAdapter;
 $NETWORKCARD = Get-WmiObject win32_networkadapterconfiguration;
 
 # DECLARATION OF FUNCTIONS
-
+function LagBehind(){
+    Start-Sleep -m 1500
+}
 # MAIN
 ## To get system specification and piping it to show only relevant data in Format table
 Write-Host "System Information" -ForegroundColor Green;
 Write-Host "";
 $COMPUTERSPEC | Format-Table Domain, Manufacturer, Model, Name, PrimaryOwnerName ,TotalPhysicalMemory;
+LagBehind;
 
 ## To get BIOS Information and piping it to show only relevant data in Format table
 Write-Host "BIOS Information" -ForegroundColor Green;
 $BIOS | Format-Table PSComputerName, Name, BiosCharacteristics, BIOSVersion, Manufacturer, ReleaseDate, SMBIOSMajorVersion, SMBIOSMinorVersion;
+LagBehind;
 
 ## To get MotherBoard Information and piping it to show only relevant data in Format table
 Write-Host "MotherBoard Information" -ForegroundColor Green;
 $MOTHERBOARD | Format-Table Manufacturer, Product, SerialNumber, Version;
+LagBehind;
 
 ## To get CPU Information and piping it to show only relevant data in Format table
 Write-Host "CPU Information" -ForegroundColor Green;
 $CPU | Format-Table Name, Number*, Manufacturer, MaxClockSpeed;
+LagBehind;
 
 ## To get Logical drive Information and piping it to show only relevant data in Format table
 Write-Host "Logical Drive Information" -ForegroundColor Green;
 $LOGICALDRIVE | Format-Table DeviceID, FreeSpace, Size,VolumeName;
+LagBehind;
 
 ## To get Physical drive Information and piping it to show only relevant data in Format table
 Write-Host "Physical Drive Information" -ForegroundColor Green;
@@ -44,13 +51,16 @@ $PHYSICALDRIVE | Format-Table -Property PSComputerName, Size, Model, MediaType;
 ## To get Total Memory Information and piping it to show only relevant data in Format table
 Write-Host "Total Memory" -ForegroundColor Green;
 $COMPUTERSPEC | Format-Table -Property TotalPhysicalMemory;
+LagBehind;
 
 ## To get network card information and piping it to show only relevant data in Format table
 Write-Host "Network Card Information" -ForegroundColor Green;
 $NETWORK | Format-Table Name, ServiceName, MACAddress, AdapterType;
+LagBehind;
 
 ## To get network card setting information and piping it to show only relevant data in Format table
 Write-Host "Network Card Settings"
 $NETWORKCARD | Format-Table ServiceName, IPAddress, DefaultIPGateway, DHCPEnabled, Description;
+LagBehind;
 
 # END
---
+++ Personal/powershell/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace powershell_spec
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            for(int i =0 ; i < 10; i++)
+            {
+                Console.WriteLine("mochi");
+            }
+        }
+    }
+}
