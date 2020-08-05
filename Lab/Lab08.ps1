c6245c8a91af4cba2535d921fcee1cfd609a5faa
jinwoov <jinwoov@gmail.com>
2020-08-05 13:57:28 -0700

    sofar working

--- Lab/Lab08.ps1
+++ Lab/Lab08.ps1
@@ -4,11 +4,27 @@
 # Description of purpose: To output system specification to text file and condense it using select-string method.
 
 # DECLARATION OF VARIABLES
-$user = $(whoami)
-# $file = New-Item -type file "c:\"
+$user = $env:UserName
+## output files
+$twentyFourFile = New-Item -type file "c:\Users\$user\Desktop\last_24.txt" -Force
+$errorFile = New-Item -type file "c:\Users\$user\Desktop\errors.txt" -Force
+## Getting Event logs
+$twentyFour = Get-EventLog -LogName System -After (Get-Date).AddDays(-1)
+$justError = Get-EventLog -LogName System -EntryType Error
+$eventID = Get-EventLog -LogName System -InstanceId 16
+$LastTwentyLogs = Get-EventLog -LogName System -Newest 20
 
 # DECLARATION OF FUNCTIONS
-
+function WriteGreen($stringT)
+{
+    Write-Host $stringT -ForegroundColor Green
+}
 # MAIN
+## Getting all of the events that happened in last 24 hours
+$twentyFour | Out-File $twentyFourFile
+$justError | Out-File $errorFile
+WriteGreen("This is event ID")
+$eventID
+WriteGreen("This is recent 20 entries from system event")
+$LastTwentyLogs
 
-$user
